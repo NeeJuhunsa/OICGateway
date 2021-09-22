6f64ea8daeeb2c5f36f1ef676cd297dc8981fbae
sansanee.soithong@gmail.com <sansanee.soithong@gmail.com>
2021-09-22 17:35:34 +0700

    test

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const PORT = process.env.PORT || 8082
 
 
 app.get("/", (req, res)=>{
-    res.json({result: "ok", data:[1,2,3,4,5]})
+    res.json({result: "ok", data:[1,2,3,4,5,9]})
 })
 
 app.listen(PORT, ()=>{
     console.log(`Serer is running. ${PORT}`)
 })
+
+
+
+
