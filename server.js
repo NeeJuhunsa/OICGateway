bd979636ba3521d5f49c5bd4c0de64e9f5adf7b1
sansanee.soithong@gmail.com <sansanee.soithong@gmail.com>
2021-09-23 23:37:53 +0700

    add axios

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express")
+//const axios = require("axios").create({baseUrl: "https://jsonplaceholder.typicode.com"});
+const axios = require('axios').default;
 const app = express()
 const PORT = process.env.PORT || 8082
 
@@ -6,6 +8,18 @@ const PORT = process.env.PORT || 8082
 app.get("/", (req, res)=>{
     res.json({result: "ok", data:[1,2,3,4,5,9,10]})
 })
+
+app.get("/async", async (req, res) => {
+	try {
+
+        const URL = "https://jsonplaceholder.typicode.com/posts"
+        const response = await axios.get(URL);
+		res.status(200).json(response.data);
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+});
+
 
 app.listen(PORT, ()=>{
     console.log(`Serer is running. ${PORT}`)
